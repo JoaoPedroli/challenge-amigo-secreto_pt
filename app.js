f205e46dfe49e92e7f1f73ab8133eebe92320bea
JoaoPedroli <joaopedrocz12345@gmail.com>
2025-01-15 20:42:38 -0300

    upgrade some things

--- app.js
+++ app.js
@@ -8,16 +8,28 @@ const getRandomInt = (max) => {
   return Math.floor(Math.random() * max);
 };
 
+const adicionarAmigoEl = (name) => {
+  listaAmigosEl.innerHTML += `<li>${name}</li>`;
+};
+
 const adicionarAmigo = () => {
   const name = inputEl.value;
-  names.push(name);
-  listaAmigosEl.innerHTML += `<li>${name}</li>`;
-  inputEl.value = "";
+  if(name === "") {
+    alert("Por favor, insira um nome.");
+  } else {
+    names.push(name);
+    adicionarAmigoEl(name);
+    inputEl.value = "";
+  }
 };
 
 const sortearAmigo = () => {
-  const randomIndex = getRandomInt(names.length);
-  resultadoEl.innerHTML = names[randomIndex];
-  listaAmigosEl.innerHTML = "";
-  names = [];
+  if(names.length === 0) {
+    alert("A lista de amigos está vazia!");
+  } else {
+    const randomIndex = getRandomInt(names.length);
+    resultadoEl.innerHTML = names[randomIndex];
+    listaAmigosEl.innerHTML = "";
+    names = [];
+  }
 };
